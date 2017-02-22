29d6bce3dddff551e97b76eca147ad82b90f5144
linus <linus.albertsson@hotmail.se>
2017-02-22 12:37:29 +0100

    Ändrat flödet i menyn

--- Megastore/pages/SubCategory.aspx
+++ Megastore/pages/SubCategory.aspx
@@ -1,12 +1,11 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeBehind="Products.aspx.cs" Inherits="Megastore.pages.store.Products" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SubCategory.aspx.cs" Inherits="Megastore.pages.SubCategory" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="title" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 
 <asp:Content ID="Content4" ContentPlaceHolderID="contentBody" runat="server">
-    
-        <asp:Label ID="testLabel" runat="server" Text="Label"></asp:Label>
+    <asp:Label ID="testLabel" runat="server" Text="Label"></asp:Label>
         <asp:GridView ID="GridView1" runat="server" Width="100%" DataSourceID="sqlTest" BackColor="#CCCCCC" BorderColor="#999999" BorderStyle="Solid" BorderWidth="3px" CellPadding="4" CellSpacing="2" ForeColor="Black">
             <FooterStyle BackColor="#CCCCCC" />
             <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
@@ -18,14 +17,13 @@
             <SortedDescendingCellStyle BackColor="#CAC9C9" />
             <SortedDescendingHeaderStyle BackColor="#383838" />
         </asp:GridView>
-        <asp:SqlDataSource ID="sqlTest" runat="server" ConnectionString="<%$ ConnectionStrings:mydbConnectionString %>" ProviderName="<%$ ConnectionStrings:mydbConnectionString.ProviderName %>" SelectCommand="SELECT product_type.type_name AS Type, product.product_name AS Name, product.product_description AS Description, product.unit AS Unit, product.price_per_unit AS Price, product.image_url AS Image, categories.category_name AS Category FROM product_type INNER JOIN product ON product_type.product_type_id = product.product_type_idproduct_type INNER JOIN categories ON product.categories_catogories_id = categories.categories_id WHERE (product_type.type_name = @category) ">
+        <asp:SqlDataSource ID="sqlTest" runat="server" ConnectionString="<%$ ConnectionStrings:mydbConnectionString %>" ProviderName="<%$ ConnectionStrings:mydbConnectionString.ProviderName %>" SelectCommand="SELECT product_type.type_name AS Type, product.product_name AS Name, product.product_description AS Description, product.unit AS Unit, product.price_per_unit AS Price, product.image_url AS Image, categories.category_name AS Category FROM product_type INNER JOIN product ON product_type.product_type_id = product.product_type_idproduct_type INNER JOIN categories ON product.categories_catogories_id = categories.categories_id WHERE (product_type.type_name = @category AND categories.category_name = @subcat) ">
              <SelectParameters>
     <asp:QueryStringParameter Name="category" QueryStringField="category" />
-                 
+                 <asp:QueryStringParameter Name="subcat" QueryStringField="subcat" />
   </SelectParameters>
         </asp:SqlDataSource>
     
         <asp:EntityDataSource ID="productData" runat="server" ConnectionString="name=mydbEntities" DefaultContainerName="mydbEntities" EnableFlattening="False" EntitySetName="products" >
         </asp:EntityDataSource>
-   
 </asp:Content>
---
+++ Megastore/pages/SubCategory.aspx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Megastore.pages
+{
+    public partial class SubCategory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["category"].Equals("Game"))
+            {
+                testLabel.Text = "Här kommer allt i games att hamna";
+                if (Request.QueryString["subcat"] != null)
+                {
+
+                    if (Request.QueryString["subcat"].Equals("Fps"))
+                    {
+                        testLabel.Text = "Här kommer allt i fps att hamna";
+                    }
+                    if (Request.QueryString["subcat"].Equals("RPG"))
+                    {
+                        testLabel.Text = "Här kommer allt i rpg att hamna";
+                    }
+                    if (Request.QueryString["subcat"].Equals("Strategy"))
+                    {
+                        testLabel.Text = "Här kommer allt i strategi att hamna";
+                    }
+                }
+            }
+
+            //Smartphones
+            if (Request.QueryString["category"].Equals("Smartphone"))
+            {
+                testLabel.Text = "Här kommer allt i smartphones att hamna";
+                if (Request.QueryString["subcat"] != null)
+                {
+                    if (Request.QueryString["subcat"].Equals("Iphone"))
+                    {
+                        testLabel.Text = "Här kommer allt i iphone att hamna";
+                    }
+                    if (Request.QueryString["subcat"].Equals("Android"))
+                    {
+                        testLabel.Text = "Här kommer allt i android att hamna";
+                    }
+                    if (Request.QueryString["subcat"].Equals("Blackberry"))
+                    {
+                        testLabel.Text = "Här kommer allt i blackberry att hamna";
+                    }
+                }
+
+            }
+
+            //Böcker
+            if (Request.QueryString["category"].Equals("Book"))
+            {
+                testLabel.Text = "Här kommer allt i books att hamna";
+
+                if (Request.QueryString["subcat"] != null)
+                {
+                    if (Request.QueryString["subcat"].Equals("Manga"))
+                    {
+                        testLabel.Text = "Här kommer allt i manga att hamna";
+                    }
+                    if (Request.QueryString["subcat"].Equals("Children"))
+                    {
+                        testLabel.Text = "Här kommer allt i children(books) att hamna";
+                    }
+                    if (Request.QueryString["subcat"].Equals("Thriller"))
+                    {
+                        testLabel.Text = "Här kommer allt i thriller(books) att hamna";
+                    }
+                }
+
+            }
+
+            //Dator
+            if (Request.QueryString["category"].Equals("Computer"))
+            {
+
+                testLabel.Text = "Här kommer allt i computers att hamna";
+                if (Request.QueryString["subcat"] != null)
+                {
+                    if (Request.QueryString["subcat"].Equals("Processor"))
+                    {
+                        testLabel.Text = "Här kommer allt i processor att hamna";
+                    }
+
+                    if (Request.QueryString["subcat"].Equals("Graphics"))
+                    {
+                        testLabel.Text = "Här kommer allt i Graphics att hamna";
+                    }
+
+                    if (Request.QueryString["subcat"].Equals("Ram"))
+                    {
+                        testLabel.Text = "Här kommer allt i RAM att hamna";
+                    }
+
+                    if (Request.QueryString["subcat"].Equals("Motherboard"))
+                    {
+                        testLabel.Text = "Här kommer allt i motherboard att hamna";
+                    }
+
+                    if (Request.QueryString["subcat"].Equals("Harddrive"))
+                    {
+                        testLabel.Text = "Här kommer allt i hdd/ssd att hamna";
+                    }
+                }
+            }
+
+            //Filmer
+            if (Request.QueryString["category"].Equals("Movie"))
+            {
+                testLabel.Text = "Här kommer allt i Movies att hamna";
+                if (Request.QueryString["subcat"] != null)
+                {
+
+                    if (Request.QueryString["subcat"].Equals("Action"))
+                    {
+                        testLabel.Text = "Här kommer allt i action att hamna";
+                    }
+
+                    if (Request.QueryString["subcat"].Equals("Thriller"))
+                    {
+                        testLabel.Text = "Här kommer allt i thriller(movies) att hamna";
+                    }
+
+                    if (Request.QueryString["subcat"].Equals("Children"))
+                    {
+                        testLabel.Text = "Här kommer allt i children(movies) att hamna";
+                    }
+                }
+            }
+        }
+    }
+}
