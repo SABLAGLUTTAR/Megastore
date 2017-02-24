9dcbca89f58fefa5f957e6aa48c4daf2ffe5e134
kevinhi <kevinhildebrand1@gmail.com>
2017-02-24 21:01:58 +0100

    login

--- Megastore/LoginUser.ascx.cs
+++ Megastore/LoginUser.ascx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -23,7 +24,40 @@ namespace Megastore
 
         protected void OnAuthenticate(object sender, AuthenticateEventArgs e)
         {
-            
+
+            string connStr = ConfigurationManager.ConnectionStrings["dataConn"].ToString();
+            using (MySql.Data.MySqlClient.MySqlConnection con = new MySql.Data.MySqlClient.MySqlConnection(connStr))
+            {
+                string username = Login1.UserName.ToString();
+                string password = Login1.Password.ToString();
+                MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand("select customer_email, customer_password from customer where customer_email like @username and customer_password = @password;");
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Connection = con;
+                con.Open();
+                System.Data.DataSet ds = new System.Data.DataSet();
+                MySql.Data.MySqlClient.MySqlDataAdapter da = new MySql.Data.MySqlClient.MySqlDataAdapter(cmd);
+                da.Fill(ds);
+                con.Close();
+
+                bool loginSuccessful = ((ds.Tables.Count > 0) && (ds.Tables[0].Rows.Count > 0));
+
+                if (loginSuccessful)
+                {
+                    Response.Cookies["user"].Value = username;
+                    Response.Cookies["user"].Expires = DateTime.Now.AddDays(1);
+                    Response.Cookies["true"].Value = "true";
+                    Response.Cookies["true"].Expires = DateTime.Now.AddDays(1);
+                    Response.Redirect("AdvancedSeach.aspx", true);
+
+                }
+                else
+                {
+                    Response.Redirect("Registration.aspx", true);
+                }
+
+
+            }
 
         }
 
