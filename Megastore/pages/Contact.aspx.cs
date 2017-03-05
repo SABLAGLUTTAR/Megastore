a325541de7f3ef5145e14c3ee5f145a654f33ee4
kevinhi <kevinhildebrand1@gmail.com>
2017-03-05 13:52:49 +0100

    Contact tillbaks

--- Megastore/pages/Contact.aspx.cs
+++ Megastore/pages/Contact.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Megastore.pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -15,41 +16,22 @@ namespace Megastore
 
         }
 
-        public void SendMail()
-        {
-            // Gmail Address from where you send the mail
-            var fromAddress = "megastorehkr@gmail.com";
-            // any address where the email will be sending
-            var toAddress = YourEmail.Text.ToString();
-            //Password of your gmail address
-            const string fromPassword = "Asdasd123";
-            // Passing the values and make a email formate to display
-            string subject = YourSubject.Text.ToString();
-            string body = "From: " + YourName.Text + "\n";
-            body += "Email: " + YourEmail.Text + "\n";
-            body += "Subject: " + YourSubject.Text + "\n";
-            body += "Question: \n" + Comments.Text + "\n";
-            // smtp settings
-            var smtp = new System.Net.Mail.SmtpClient();
-            {
-                smtp.Host = "smtp.gmail.com";
-                smtp.Port = 587;
-                smtp.EnableSsl = true;
-                smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-                smtp.Credentials = new NetworkCredential(fromAddress, fromPassword);
-                smtp.Timeout = 20000;
-            }
-            // Passing values to smtp object
-            smtp.Send(fromAddress, toAddress, subject, body);
-        }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
             try
             {
-                //here on button click what will done 
-                SendMail();
-                DisplayMessage.Text = "Your Comments after sending the mail";
+                string toAddress = YourEmail.Text.ToString();
+                string adminEmail = "megastorehkr@gmail.com"; // send to admin and user
+                string subject = YourSubject.Text.ToString();
+                string body = "From: " + toAddress+ "\n";
+                body += "Email: " + YourEmail.Text + "\n";
+                body += "Subject: " + YourSubject.Text + "\n";
+                body += "Question: \n" + Comments.Text + "\n";
+                MailClass.MailSender(adminEmail, subject, body);
+                MailClass.MailSender(toAddress, subject, body); //Copy to sender
+
+                DisplayMessage.Text = "Your message has been forwarded. A copy has been sent to your email.";
                 DisplayMessage.Visible = true;
                 YourSubject.Text = "";
                 YourEmail.Text = "";
